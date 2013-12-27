ef663082b948ca4bf352cf5c5a281d324699d18c
wiredeltainc <wd.staff@wiredelta.com>
2013-12-27 19:36:34 +0530

    updated

--- js/all.js
+++ js/all.js
@@ -163,22 +163,7 @@ $(document).ready(function(){
 document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady() {
-	var my_media = new Media('http://dev.wiredelta.com:12001/sample.mp3', onSuccess, onError);
-// Update media position every second
-var mediaTimer = setInterval(function () {
-    // get media position
-    my_media.getCurrentPosition(
-        // success callback
-        function (position) {
-            if (position > -1) {
-                console.log((position) + " sec");
-            }
-        },
-        // error callback
-        function (e) {
-            console.log("Error getting pos=" + e);
-        }
-    );
-}, 1000);
+//	var my_media = new Media('http://dev.wiredelta.com:12001/sample.mp3', onSuccess, onError);
+    playAudio("http://audio.ibeat.org/content/p1rj1s/p1rj1s_-_rockGuitar.mp3");
 }
 
