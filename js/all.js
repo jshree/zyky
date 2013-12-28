b8529863775fc65e9b2d138979ac7102d39be34f
wiredeltainc <wd.staff@wiredelta.com>
2013-12-28 12:26:13 +0530

    updated

--- js/all.js
+++ js/all.js
@@ -144,7 +144,23 @@ document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady() {
 //	var my_media = new Media('http://dev.wiredelta.com:12001/sample.mp3', onSuccess, onError);
-    my_media = new Media('http://dev.wiredelta.com:12001/sample.mp3', onSuccess, onError);
-    my_media.play();
+    var my_media = new Media('http://dev.wiredelta.com:12001/sample.mp3', onSuccess, onError);
+
+// Update media position every second
+var mediaTimer = setInterval(function () {
+    // get media position
+    my_media.getCurrentPosition(
+        // success callback
+        function (position) {
+            if (position > -1) {
+                console.log((position) + " sec");
+            }
+        },
+        // error callback
+        function (e) {
+            console.log("Error getting pos=" + e);
+        }
+    );
+}, 1000);
 }
 
