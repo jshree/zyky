d55db4c9b00cf667b30dd5789e7b1c87107f531a
ScriptMaster <scriptmaster@gmail.com>
2014-03-01 17:13:46 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,9 +9,6 @@ $('input').blur(function(){
 	$(this).parents('form').removeClass('keyboard_visible');
 });
 
-//$('.loginform').hide();
-setTimeout($('.loginform').fadeIn, 500);
-
 //a (table) view to render the list of songs
 var song_list_view = Backbone.View.extend({
     el: $('.song-temp'),
